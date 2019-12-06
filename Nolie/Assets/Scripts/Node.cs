91e78ae82a7f1eb558d466f3342045b7c58be1ac
Martin Rossel <m.rossel@student.isartdigital.com>
2019-12-06 16:19:02 +0100

    Projet Set Up

---
+++ Nolie/Assets/Scripts/Node.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Node", menuName = "Node")]
+public class Node : ScriptableObject
+{
+    [SerializeField] string name;
+    [SerializeField] int index;
+
+    [SerializeField][TextArea] string[] Texts;
+}
